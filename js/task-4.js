38ac94fde78e03c6bb82d9e5b53ad5bd1b6bcc93
Vova <Vova.gorlovoy@gmail.com>
2026-07-20 22:56:42 +0300

    update

--- js/task-4.js
+++ js/task-4.js
@@ -6,33 +6,28 @@ function getShippingCost(country) {
       let message1 = `Shipping to ${country1} will cost ${price1} credits`;
       return message1;
 
-
-      case `Chile`:
-        let country2 = 'Chile';
+    case `Chile`:
+      let country2 = 'Chile';
       let price2 = 250;
       let message2 = `Shipping to ${country2} will cost ${price2} credits`;
       return message2;
 
-
-      case `Australia`:
-        let country3 = 'Australia';
+    case `Australia`:
+      let country3 = 'Australia';
       let price3 = 170;
       let message3 = `Shipping to ${country3} will cost ${price3} credits`;
       return message3;
 
-      
-       case `Jamaica`:
-        let country4 = 'Jamaica';
+    case `Jamaica`:
+      let country4 = 'Jamaica';
       let price4 = 120;
       let message4 = `Shipping to ${country4} will cost ${price4} credits`;
       return message4;
 
-
-      default : 
-      const messageNo = "Sorry, there is no delivery to your country";
+    default:
+      const messageNo = 'Sorry, there is no delivery to your country';
       return messageNo;
   }
-
 }
 
 console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
